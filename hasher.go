8c39989a8000822acdeedfbd813b6c4014a0fb5f
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2017-11-16 17:32:09 +0530

    Added comments to exported type

--- hasher.go
+++ hasher.go
@@ -7,12 +7,15 @@ import (
 	"errors"
 )
 
+// HashService is a simple interface for a service that returns a SHA256 hash of a
+// given string
 type HashService interface {
 	HashSHA256(context.Context, string) (string, error)
 }
 
 type hashService struct{}
 
+// HashSHA256 returns SHA256 hash of the provided string
 func (hashService) HashSHA256(_ context.Context, input string) (string, error) {
 	if input == "" {
 		return "", ErrEmpty

5e9cd51ea64f4a7e174590b2df757ea1e05c008b
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2017-11-17 13:23:14 +0530

    Added wiring tests for the transport

---
+++ wiring_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+var srv *httptest.Server
+
+func init() {
+	svc := hashService{}
+	hashEndpoint := makeHashSHA256Endpoint(svc)
+	router := makeHashSHA256Handler(hashEndpoint)
+	srv = httptest.NewServer(router)
+}
+
+func TestHashWiring(t *testing.T) {
+	for _, testcase := range []struct {
+		method, url, body, want string
+	}{
+		{"POST", srv.URL + "/hash", `{"s":"world"}`, `{"v":"486ea46224d1bb4fb680f34f7c9ad96a8f24ec88be73ea8e5a6c65260e9cb8a7"}`},
+		{"POST", srv.URL + "/hash", `{"s":""}`, `{"v":"","err":"Empty input string"}`},
+		{"POST", srv.URL + "/hash", `{"sdfs":""}`, `{"v":"","err":"Empty input string"}`},
+	} {
+		req, _ := http.NewRequest(testcase.method, testcase.url, strings.NewReader(testcase.body))
+		resp, _ := http.DefaultClient.Do(req)
+		body, _ := ioutil.ReadAll(resp.Body)
+		if want, have := testcase.want, strings.TrimSpace(string(body)); want != have {
+			t.Errorf("%s %s %s: want %q, have %q", testcase.method, testcase.url, testcase.body, want, have)
+		}
+	}
+}
